e96d618598d04468c516d3e19ca7980a39db19f8
Iliya Soleimani <zalizadeh780@gmail.com>
2022-10-26 09:24:32 +0330

    Added text input

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var messageLst = [Message(message: "meow2", isSentBySelf: false), Message(message: "meow3", isSentBySelf: true), Message(message: "meow5", isSentBySelf: false)];
+  TextEditingController nameController = TextEditingController();
+  String fullName = '';
 
   @override
   Widget build(BuildContext context) {
@@ -29,28 +31,63 @@ class _MyHomePageState extends State<MyHomePage> {
             title: Text("Helli3 Messanger"),
             backgroundColor: Color(0xff627764)),
         body: Padding(
-          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
           child: Column(
             children: [
-              Container(
-                height: 500,
-                child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-                          for(var currentMessage in messageLst ) MessageWidget(message: currentMessage.message, isSentBySelf: currentMessage.isSentBySelf),
+              Expanded(child: SizedBox(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: Expanded(child: FractionallySizedBox(
+                    heightFactor: 0.9,
+                    child: Container(
+                      child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                                for(var currentMessage in messageLst ) MessageWidget(message: currentMessage.message, isSentBySelf: currentMessage.isSentBySelf),
 
-                        ]),
+                              ]),
 
-                      ],
-                    )
+                            ],
+                          )
+                      ),
+                    ),
+                  )),
                 ),
-              ),
-              TextButton(onPressed: () {
-                setState(() {
-                  messageLst.add(Message(message: "meow4", isSentBySelf: true));
-                });
-              }, child: Text("Click Me!"))
+              )),
+              Container(
+                color: Colors.grey[400],
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: Row(
+                    children: [
+                      Expanded(child: SizedBox(
+                          height: 50,
+                          child: TextField(
+                            controller: nameController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Full Name',
+                            ),
+                            onChanged: (text) {
+                              setState(() {
+                                fullName = text;
+                                //you can access nameController in its scope to get
+                                // the value of text entered as shown below
+                                //fullName = nameController.text;
+                              });
+                            },
+                          )
+                      )),
+                      TextButton(onPressed: () {
+                        setState(() {
+                          messageLst.add(Message(message: nameController.text, isSentBySelf: true));
+                        });
+                      }, child: Text("Click Me!"))
+                    ],
+                  ),
+                ),
+              )
             ],
           )
         ));
